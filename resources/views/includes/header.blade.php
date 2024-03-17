608937c544eb84afce8b8c32fe01e5bbfc47d3a8
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-17 17:52:06 +0100

    fixed header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -7,10 +7,16 @@
         </figure>
         <nav>
             <ul class="header-list">
-                @foreach (config('header_menu') as $link)
-                <li class="{{ Route::is($link['route_name']) ? 'current' : ''}}"><a href="{{$link['route_name']}}">{{$link['text']}}</a></li>
-                @endforeach
-
+                <li class="@if (Route::is('characters')) current @endif"><a href="{{route('characters')}}">Characters</a></li>
+                <li class="@if (Request::is('comics*'))) current @endif"><a href="{{route('comics.index')}}">Comics</a></li>
+                <li class=""><a href="">Movies</a></li>
+                <li class=""><a href="">Tv</a></li>
+                <li class=""><a href="">Games</a></li>
+                <li class=""><a href="">Collectibles</a></li>
+                <li class=""><a href="">Videos</a></li>
+                <li class=""><a href="">Fans</a></li>
+                <li class=""><a href="">News</a></li>
+                <li class=""><a href="">Shop</a></li>
             </ul>
         </nav>
     </div>
