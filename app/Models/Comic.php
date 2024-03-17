6f12246a5bc20dacadc4e6c84fa9aca71c6e1adf
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-17 17:12:36 +0100

    added fillable

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Comic extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'series', 'type', 'price', 'thumb', 'date', 'artists', 'writers', 'description'];
 }
