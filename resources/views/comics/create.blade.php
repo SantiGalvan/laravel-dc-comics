540e612bc78d9f6c313432d4ce572f9f78fd9ac3
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-17 18:13:27 +0100

    added value dinamic

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -17,55 +17,55 @@
                         <div class="col-5">
                             <div class="mb-3">
                                 <label for="title" class="form-label">Titolo</label>
-                                <input type="text" class="form-control" id="title" name="title">
+                                <input value="{{old('title', '')}}" type="text" class="form-control" id="title" name="title">
                             </div>
                         </div>
                         <div class="col-5">
                             <div class="mb-3">
                                 <label for="series" class="form-label">Serie</label>
-                                <input type="text" class="form-control" id="series" name="series">
+                                <input value="{{old('series', '')}}" type="text" class="form-control" id="series" name="series">
                             </div>
                         </div>
                         <div class="col-2">
                             <div class="mb-3">
                                 <label for="type" class="form-label">Tipo</label>
-                                <input type="text" class="form-control" id="type" name="type">
+                                <input value="{{old('type', '')}}" type="text" class="form-control" id="type" name="type">
                             </div>
                         </div>
                         <div class="col-3">
                             <div class="mb-3">
                                 <label for="price" class="form-label">Prezzo</label>
-                                <input type="number" class="form-control" id="price" name="price">
+                                <input value="{{old('price', '')}}" type="number" class="form-control" id="price" name="price">
                             </div>
                         </div>
                         <div class="col-6">
                             <div class="mb-3">
                                 <label for="thumb" class="form-label">Immagine</label>
-                                <input type="text" class="form-control" id="thumb" name="thumb">
+                                <input value="{{old('thumb', '')}}" type="text" class="form-control" id="thumb" name="thumb">
                             </div>
                         </div>
                         <div class="col-3">
                             <div class="mb-3">
                                 <label for="date" class="form-label">Data</label>
-                                <input type="text" class="form-control" id="date" name="sale_date">
+                                <input value="{{old('date', '')}}" type="text" class="form-control" id="date" name="sale_date">
                             </div>
                         </div>
                         <div class="col-6">
                             <div class="mb-3">
                                 <label for="artists" class="form-label">Artisti</label>
-                                <input type="text" class="form-control" id="artists" name="artists">
+                                <input value="{{old('artists', '')}}" type="text" class="form-control" id="artists" name="artists">
                             </div>
                         </div>
                         <div class="col-6">
                             <div class="mb-3">
                                 <label for="writers" class="form-label">Scrittori</label>
-                                <input type="text-area" class="form-control" id="writers" name="writers">
+                                <input value="{{old('writers', '')}}" type="text-area" class="form-control" id="writers" name="writers">
                             </div>
                         </div>
                         <div class="offset-2 col-8">
                             <div class="mb-3">
                                 <label for="description" class="form-label">Descrizione</label>
-                                <textarea class="form-control" id="description" name="description" rows="5"></textarea>
+                                <textarea class="form-control" id="description" name="description" rows="5">{{old('description', '')}}</textarea>
                             </div>
                         </div>
                     </div>
