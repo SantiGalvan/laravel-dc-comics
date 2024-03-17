1dc258b7b7136b09e01a778993c5e084b483a553
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-17 19:13:54 +0100

    fixed routes

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,11 @@ Route::get('/', function () {
 Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
 // Route per la creazione
 Route::get('comics/create', [ComicController::class, 'create'])->name('comics.create');
-// Route per la modifica
-Route::get('comics/{comic}/edit', [ComicController::class, 'edit'])->name('comics.edit');
 // Route Comic
 Route::get('/comics/{comic}', [ComicController::class, 'show'])->name('comics.show');
+// Route per la modifica
+Route::get('comics/{comic}/edit', [ComicController::class, 'edit'])->name('comics.edit');
+
 // Rotta per il salvataggio
 Route::post('/comics', [ComicController::class, 'store'])->name('comics.store');
 
