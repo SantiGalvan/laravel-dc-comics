8efd0247d9ed45c458b771d74e8af34717ec6e9e
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-18 18:56:11 +0100

    added with

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -92,6 +92,6 @@ class ComicController extends Controller
     {
         $comic->delete();
 
-        return to_route('comics.index');
+        return to_route('comics.index')->with('message', 'Comic eliminato con successo');
     }
 }
