cf209969ec3047494353595751a3533a9385bbc3
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-18 21:41:03 +0100

    created route trash

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,8 @@ Route::get('/', function () {
 Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
 // Route per la creazione
 Route::get('comics/create', [ComicController::class, 'create'])->name('comics.create');
+// Rotta per il cestino
+Route::get('/comics/trash', [ComicController::class, 'trash'])->name('comics.trash');
 // Route Comic
 Route::get('/comics/{comic}', [ComicController::class, 'show'])->name('comics.show');
 // Route per la modifica
@@ -36,6 +38,10 @@ Route::post('/comics', [ComicController::class, 'store'])->name('comics.store');
 
 // Rotta per la cancellazione 
 Route::delete('/comics/{comic}', [ComicController::class, 'destroy'])->name('comics.destroy');
+
+// Rotta generica, crea tutte le rotte senza doverle farle a mano
+// Route::resource('comics', ComicController::class);
+
 
 // Route Characters
 Route::get('/characters', function () {
